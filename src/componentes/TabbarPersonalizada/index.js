c698effc307183306869426b50fb5febd70657f5
Wilson Ramos <hupcontato@gmail.com>
2026-08-23 15:02:41 -0300

    Commit

---
+++ src/componentes/TabbarPersonalizada/index.js
@@ -0,0 +1,89 @@
+import { TouchableOpacity, View, StyleSheet } from 'react-native';
+import Ionicons from 'react-native-vector-icons/Ionicons';
+
+export default function TabbarPersonalizada({ state, descriptors, navigation }) {
+  return (
+    <View style={styles.container}>
+
+      <View style={styles.content}>
+        {state.routes.map((route, index) => {
+          const { options } = descriptors[route.key]
+
+          const isFocused = state.index === index
+
+          const onPress = () => {
+            const event = navigation.emit({
+              type: 'tabPress',
+              target: route.key,
+              canPreventDefault: true
+            })
+
+            if (!isFocused && !event.canPreventDefault) {
+              navigation.navigate(route.name, {}, { merge: true })
+            }
+          }
+
+          const onLongPress = () => {
+            navigation.emit({
+              type: "tabLongPress",
+              target: route.key
+            })
+          }
+
+          return (
+            <TouchableOpacity
+              key={index}
+              accessibilityRole='button'
+              accessibilityState={isFocused ? { selected: true } : {}}
+              accessibilityLabel={options.tabBarAccessibilityLabel}
+              testID={options.tabBarTestID}
+              onPress={onPress}
+              onLongPress={onLongPress}
+              style={styles.buttonTab}
+            >
+
+              <View style={{ alignItems: "center", padding: 2, }}>
+                <View style={{ padding: 14, backgroundColor: isFocused ? '#ff0000' : '#ffffffff', borderRadius: 30 }}>
+
+                  <Ionicons name={options.tabBarIcon} size={24} color={isFocused ? "#fff" : '#00000035'} />
+
+                </View>
+              </View>
+
+            </TouchableOpacity>
+          )
+        })}
+      </View>
+    </View>
+
+  );
+
+
+}
+
+const styles = StyleSheet.create({
+
+  container: {
+    alignItems: "center",
+    justifyContent: "center",
+    backgroundColor: 'blue'
+
+  },
+  content: {
+    flexDirection: "row",
+    marginBottom: 24,
+    alignItems: 'center',
+    justifyContent: "center",
+    position: "absolute",
+    marginBottom: 28,
+    bottom: 0,
+    backgroundColor: 'rgba(255,255,255)',
+    borderRadius: 30,
+    elevation: 5,
+  },
+  buttonTab: {
+    alignItems: "center",
+    justifyContent: "center",
+
+  }
+})
